b7d09306db501d8d35e9ad24e79a880e45dec24c
ivan.kishchenko <ivan.kishchenko@aspose.com>
2020-11-25 00:44:57 +0300

    Implement salve and adrenalin

--- src/scripts/4_World/Static/TurnSyringeFillLambda.c
+++ src/scripts/4_World/Static/TurnSyringeFillLambda.c
@@ -7,7 +7,16 @@ modded class TurnSyringeFillLambda
         SyringeFull syringeFull = SyringeFull.Cast(new_item);
         if (syringeFull)
         {
-            syringeFull.UpdateSyringeData(m_medSolutionClassname, m_dirty);
+            syringeFull.UpdateSyringeData(m_medSolutionItem.GetType(), m_dirty);
+        }
+		
+		if (m_medSolutionItem.HasQuantity())
+        {
+            m_medSolutionItem.AddQuantity(-1,true);
+        }
+        else
+        {
+            m_medSolutionItem.Delete();
         }
     }
 };
