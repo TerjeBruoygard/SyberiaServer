a5e381f6e36f913d83c4a5e7c901b9b3a9b4c519
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-11-22 16:02:28 +0300

    Fix

--- src/scripts/4_World/Entities/ItemBase/PetrolLighter.c
+++ src/scripts/4_World/Entities/ItemBase/PetrolLighter.c
@@ -3,14 +3,24 @@ modded class PetrolLighter
     override void OnIgnitedTarget( EntityAI ignited_item )
 	{
         PlayerBase player = PlayerBase.Cast(GetHierarchyRootPlayer());
-        float value = (float)player.GetPerkIntValue(SyberiaPerkType.SYBPERK_SURVIVAL_TOOLS_DEGRADATION_DEC, 1);
+        float value = 1.0;
+        if (player)
+        {
+            value = (float)player.GetPerkIntValue(SyberiaPerkType.SYBPERK_SURVIVAL_TOOLS_DEGRADATION_DEC, 1);
+        }
+        
         AddQuantity( -1.0 / value );
 	}
 	
 	override void OnIgnitedTargetFailed( EntityAI target_item )
 	{
         PlayerBase player = PlayerBase.Cast(GetHierarchyRootPlayer());
-        float value = (float)player.GetPerkIntValue(SyberiaPerkType.SYBPERK_SURVIVAL_TOOLS_DEGRADATION_DEC, 1);
+        float value = 1.0;
+        if (player)
+        {
+            value = (float)player.GetPerkIntValue(SyberiaPerkType.SYBPERK_SURVIVAL_TOOLS_DEGRADATION_DEC, 1);
+        }
+        
         AddQuantity( -1.0 / value );
 	}
 };
