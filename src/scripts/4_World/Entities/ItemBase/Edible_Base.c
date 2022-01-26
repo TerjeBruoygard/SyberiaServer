6e1299662fe9c3ba0286d939a9a8f83471add041
ivan.kishchenko <ivan.kishchenko@aspose.com>
2022-01-26 17:04:40 +0300

    Fix animals skinning

--- src/scripts/4_World/Entities/ItemBase/Edible_Base.c
+++ src/scripts/4_World/Entities/ItemBase/Edible_Base.c
@@ -63,106 +63,110 @@ modded class Edible_Base
 		ItemBase added_item;
 		float meatCountMod = butcher.GetPerkFloatValue(SyberiaPerkType.SYBPERK_HUNTING_MEAT_COUNT, 0, 0);
 		float skinningMod = butcher.GetPerkFloatValue(SyberiaPerkType.SYBPERK_HUNTING_SKINING, 0, 0);
-		string skinningCfg = CFG_VEHICLESPATH + " " + GetType() + " Skinning ";
+		string skinningCfg = CFG_VEHICLESPATH + " " + GetType() + " Skinning";
+		int skinningChildsCount = GetGame().ConfigGetChildrenCount(skinningCfg);
 		string itemName;
 		float count;
 		float quantityMin, quantityMax;
+		string skinningChildName;
 		
-		if (GetGame().ConfigIsExisting(skinningCfg + "ObtainedSteaks"))
+		for (int childId = 0; childId < skinningChildsCount; childId++)
 		{
-			itemName = GetGame().ConfigGetTextOut(skinningCfg + "ObtainedSteaks item");
-			count = GetGame().ConfigGetFloat(skinningCfg + "ObtainedSteaks count");
-			if (count > 0) 
+			if (GetGame().ConfigGetChildName(skinningCfg, childId, skinningChildName))
 			{
-				quantityMin = Math.Clamp(0.2 + meatCountMod, 0.2, 0.9);
-				quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);			
-				count = Math.Max((count * (meatCountMod + 0.2)) + 1, 1);
-				
-				while (count > 0) {
-					count = count - 1.0;
-					added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
-					if (added_item) {
-						added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
-						added_item.InsertAgent(eAgents.SALMONELLA, 1);
-						added_item.SetTemperature(38);
+				itemName = GetGame().ConfigGetTextOut(skinningCfg + " " + skinningChildName + " item");
+				count = GetGame().ConfigGetFloat(skinningCfg + " " + skinningChildName + " count");
+				if (skinningChildName == "ObtainedSteaks")
+				{
+					if (count > 0) 
+					{
+						quantityMin = Math.Clamp(0.2 + meatCountMod, 0.2, 0.9);
+						quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);			
+						count = Math.Max((count * (meatCountMod + 0.2)) + 1, 1);
+						
+						while (count > 0) {
+							count = count - 1.0;
+							added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
+							if (added_item) {
+								added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
+								added_item.InsertAgent(eAgents.SALMONELLA, 1);
+								added_item.SetTemperature(38);
+							}
+						}
 					}
 				}
-			}
-		}
-		
-		if (meatCountMod > 0.3 && GetGame().ConfigIsExisting(skinningCfg + "ObtainedLard"))
-		{
-			itemName = GetGame().ConfigGetTextOut(skinningCfg + "ObtainedLard item");
-			count = GetGame().ConfigGetFloat(skinningCfg + "ObtainedLard count");
-			if (count > 0) 
-			{
-				quantityMin = Math.Clamp(0.1 + meatCountMod, 0.1, 0.9);
-				quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);
-				
-				while (count > 0) {
-					count = count - 1.0;
-					added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
-					if (added_item) {
-						added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
-						added_item.InsertAgent(eAgents.SALMONELLA, 1);
-						added_item.SetTemperature(38);
+				else if (skinningChildName == "ObtainedLard")
+				{
+					if (meatCountMod > 0.3 && count > 0) 
+					{
+						quantityMin = Math.Clamp(0.1 + meatCountMod, 0.1, 0.9);
+						quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);
+						
+						while (count > 0) {
+							count = count - 1.0;
+							added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
+							if (added_item) {
+								added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
+								added_item.InsertAgent(eAgents.SALMONELLA, 1);
+								added_item.SetTemperature(38);
+							}
+						}
 					}
 				}
-			}
-		}
-		
-		if (GetGame().ConfigIsExisting(skinningCfg + "ObtainedBones"))
-		{
-			itemName = GetGame().ConfigGetTextOut(skinningCfg + "ObtainedBones item");
-			count = GetGame().ConfigGetFloat(skinningCfg + "ObtainedBones count");	
-			if (count > 0) 
-			{	
-				count = Math.Max((count * (meatCountMod + 0.5)), 1);
-				quantityMin = Math.Clamp(0.2 + meatCountMod, 0.2, 0.9);
-				quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);
-				
-				while (count > 0) {
-					count = count - 1.0;
-					added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
-					if (added_item) {
-						added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
-						added_item.InsertAgent(eAgents.SALMONELLA, 1);
-						added_item.SetTemperature(38);
-						added_item.SetHealth01("", "", meatCountMod);
+				else if (skinningChildName == "ObtainedBones")
+				{
+					if (count > 0) 
+					{	
+						count = Math.Max((count * (meatCountMod + 0.5)), 1);
+						quantityMin = Math.Clamp(0.2 + meatCountMod, 0.2, 0.9);
+						quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);
+						
+						while (count > 0) {
+							count = count - 1.0;
+							added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
+							if (added_item) {
+								added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
+								added_item.InsertAgent(eAgents.SALMONELLA, 1);
+								added_item.SetTemperature(38);
+								added_item.SetHealth01("", "", meatCountMod);
+							}
+						}
 					}
 				}
-			}
-		}
+				else if (skinningChildName == "ObtainedGuts")
+				{
+					if (count > 0) 
+					{
+						quantityMin = Math.Clamp(0.2 + meatCountMod, 0.2, 0.9);
+						quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);
 		
-		if (GetGame().ConfigIsExisting(skinningCfg + "ObtainedGuts"))
-		{
-			itemName = GetGame().ConfigGetTextOut(skinningCfg + "ObtainedGuts item");
-			count = GetGame().ConfigGetFloat(skinningCfg + "ObtainedGuts count");
-			if (count > 0) 
-			{
-				quantityMin = Math.Clamp(0.2 + meatCountMod, 0.2, 0.9);
-				quantityMax = Math.Clamp(0.3 + meatCountMod, 0.3, 1.0);
-
-				while (count > 0) {
-					count = count - 1.0;
-					added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
-					if (added_item) {
-						added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
-						added_item.InsertAgent(eAgents.SALMONELLA, 1);
-						added_item.SetTemperature(38);
-						added_item.SetHealth01("", "", meatCountMod);
+						while (count > 0) {
+							count = count - 1.0;
+							added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
+							if (added_item) {
+								added_item.SetQuantity(Math.Round(Math.RandomFloat(quantityMin, quantityMax) * added_item.GetQuantityMax()), false);
+								added_item.InsertAgent(eAgents.SALMONELLA, 1);
+								added_item.SetTemperature(38);
+								added_item.SetHealth01("", "", meatCountMod);
+							}
+						}
 					}
 				}
-			}
-		}
-		
-		if (skinningMod > 0 && GetGame().ConfigIsExisting(skinningCfg + "ObtainedPelt"))
-		{
-			itemName = GetGame().ConfigGetTextOut(skinningCfg + "ObtainedPelt item");
-			added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
-			if (added_item) {
-				added_item.SetTemperature(38);
-				added_item.SetHealth01("", "", skinningMod);
+				else if (skinningChildName == "ObtainedPelt")
+				{
+					added_item = ItemBase.Cast(GetInventory().CreateInInventory(itemName));
+					if (added_item) {
+						added_item.SetTemperature(38);
+						added_item.SetHealth01("", "", skinningMod);
+					}
+				}
+				else
+				{
+					while (count > 0) {
+						count = count - 1.0;
+						ItemBase.Cast(GetInventory().CreateInInventory(itemName));
+					}
+				}
 			}
 		}
 		
