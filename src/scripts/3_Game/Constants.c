4978485af241a47223af9e0a76c519e565ca1940
ivan.kishchenko <ivan.kishchenko@aspose.com>
2022-02-17 17:05:32 +0300

    Update version

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,9 +1,9 @@
 const string SyberiaServer_ModPreffix = "[SYBERIA SERVER] ";
 const bool SyberiaServer_DebugMode = true;
-const string Syberia_ServerVersion = "OPEN BETA 0.49L";
+const string Syberia_ServerVersion = "OPEN BETA 0.49M";
 
 modded class SyberiaVersionChecker {
-	override void CLIENT_AND_SERVER_MODS_VERSION_MISSMATCH_0_49L() {}
+	override void CLIENT_AND_SERVER_MODS_VERSION_MISSMATCH_0_49M() {}
 }
 
 void SybLogSrv(string message)
