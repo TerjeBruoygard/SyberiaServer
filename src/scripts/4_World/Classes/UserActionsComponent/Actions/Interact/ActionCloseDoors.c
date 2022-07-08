46375eb001c40f862631015ac6899bb3967f340c
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:53:42 +0400

    Improve doors management for base building

--- src/scripts/4_World/Classes/UserActionsComponent/Actions/Interact/ActionCloseDoors.c
+++ src/scripts/4_World/Classes/UserActionsComponent/Actions/Interact/ActionCloseDoors.c
@@ -8,12 +8,34 @@ modded class ActionCloseDoors
 			int doorIndex = building.GetDoorIndex(action_data.m_Target.GetComponentIndex());
 			if ( doorIndex != -1 )
 			{
-				building.CloseDoor(doorIndex);
+				int livespaceDoorIndex = -1;
+				BuildingLivespace livespace = null;
 				if (building.IsInherited(BuildingLivespace))
 				{
-					BuildingLivespace.Cast(building).CloseDoorLinked(doorIndex);
+					livespace = BuildingLivespace.Cast(building);
+					livespaceDoorIndex = livespace.FindLivespaceDoorIdBySelfDoorIndex(doorIndex);
+				}
+				
+				PluginBuildingSystem buildingPlugin = PluginBuildingSystem.Cast(GetPlugin(PluginBuildingSystem));
+				if (buildingPlugin && livespace == null)
+				{
+					livespace = buildingPlugin.FindByHousePoint(building, building.GetDoorSoundPos(doorIndex));
+					if (livespace != null)
+					{
+						livespaceDoorIndex = livespace.FindLivespaceDoorIdByLinkedDoorIndex(doorIndex);
+					}
+				}
+				
+				if (livespace != null && livespaceDoorIndex != -1)
+				{
+					if (livespace.CloseLivespaceDoor(livespaceDoorIndex))
+					{
+						return;
+					}
 				}
 			}
 		}
+		
+		super.OnStartServer(action_data);
 	}
 };
--- src/scripts/4_World/Classes/UserActionsComponent/Actions/Interact/ActionLockedDoors.c
+++ src/scripts/4_World/Classes/UserActionsComponent/Actions/Interact/ActionLockedDoors.c
@@ -1,4 +1,41 @@
 modded class ActionLockedDoors
 {
-
+	override void OnExecute( ActionData action_data )
+	{
+		House building;
+		if ( Class.CastTo(building, action_data.m_Target.GetObject()) )
+		{
+			int doorIndex = building.GetDoorIndex(action_data.m_Target.GetComponentIndex());
+			if ( doorIndex != -1 )
+			{
+				int livespaceDoorIndex = -1;
+				BuildingLivespace livespace = null;
+				if (building.IsInherited(BuildingLivespace))
+				{
+					livespace = BuildingLivespace.Cast(building);
+					livespaceDoorIndex = livespace.FindLivespaceDoorIdBySelfDoorIndex(doorIndex);
+				}
+				
+				PluginBuildingSystem buildingPlugin = PluginBuildingSystem.Cast(GetPlugin(PluginBuildingSystem));
+				if (buildingPlugin && livespace == null)
+				{
+					livespace = buildingPlugin.FindByHousePoint(building, building.GetDoorSoundPos(doorIndex));
+					if (livespace != null)
+					{
+						livespaceDoorIndex = livespace.FindLivespaceDoorIdByLinkedDoorIndex(doorIndex);
+					}
+				}
+				
+				if (livespace != null && livespaceDoorIndex != -1)
+				{
+					if (livespace.OpenLivespaceDoor(livespaceDoorIndex))
+					{
+						return;
+					}
+				}
+			}
+		}
+		
+		super.OnExecute(action_data);
+	}
 };
