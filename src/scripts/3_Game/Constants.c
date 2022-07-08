f63ae1e56bbf7efde5553373c3fce7f2e1dfaff2
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:54:04 +0400

    Update version

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string SyberiaServer_ModPreffix = "[SYBERIA SERVER] ";
 const bool SyberiaServer_DebugMode = true;
-const string Syberia_ServerVersion = "OPEN BETA 0.49K";
+const string Syberia_ServerVersion = "OPEN BETA 0.49L";
 
 modded class SyberiaVersionChecker {
 	override void CLIENT_AND_SERVER_MODS_VERSION_MISSMATCH_0_49K() {}
