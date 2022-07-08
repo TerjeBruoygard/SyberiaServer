ed2c1869a55196f60457c340b47a093fa91c1e26
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:52:17 +0400

    Commit SyberiaServerManager

---
+++ manager/CommandLineOptions.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+
+namespace SyberiaServerManager
+{
+    public class CommandLineOptions
+    {
+        [Option('p', "port", Required = false, Default = Constants.DEFAULT_SERVER_PORT, HelpText = "Sets the server port.")]
+        public int Port { get; set; }
+
+        [Option("no-logs", Required = false, Default = false, HelpText = "Disable logging to file and output.")]
+        public bool NoLogs { get; set; }
+
+        [Option("debug", Required = false, Default = false, HelpText = "Enable debug logging (Print all queries to the log).")]
+        public bool Debug { get; set; }
+
+        public CommandLineOptions() { }
+
+        public static void Parse(string[] args, Action<CommandLineOptions> result)
+        {
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(result);
+        }
+    }
+}
