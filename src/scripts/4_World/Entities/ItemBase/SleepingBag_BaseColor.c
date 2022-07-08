10e4b6c989472770c9cb3c8b0e01642f45f7b524
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:50:14 +0400

    Implement sleeping process

---
+++ src/scripts/4_World/Entities/ItemBase/SleepingBag_BaseColor.c
@@ -0,0 +1,43 @@
+modded class SleepingBagPlaced_BaseColor
+{
+	protected ref Timer m_HeatingTimer;
+	
+	override void EEInit()
+	{
+		super.EEInit();
+		if (!m_HeatingTimer)
+		{
+			m_HeatingTimer = new Timer ( CALL_CATEGORY_GAMEPLAY );
+			m_HeatingTimer.Run ( FireplaceBase.TIMER_HEATING_UPDATE_INTERVAL, this, "Heating", NULL, true );	
+		}
+	}
+	
+	override void EEDelete(EntityAI parent)
+	{
+		super.EEDelete(parent);
+		if (m_HeatingTimer)
+		{
+			m_HeatingTimer.Stop();
+			m_HeatingTimer = null;
+		}
+	}
+	
+	protected void Heating()
+	{
+		array<Object> nearest_objects = new array<Object>;
+		GetGame().GetObjectsAtPosition ( GetPosition(), 0.5, nearest_objects, null ); 
+
+		for ( int i = 0; i < nearest_objects.Count(); i++ )
+		{
+			Object nearest_object = nearest_objects.Get(i);
+			if ( nearest_object.IsInherited( PlayerBase ) )
+			{
+				PlayerBase player = PlayerBase.Cast( nearest_object );
+				if (player.m_sleepingLevel != 0)
+				{
+					player.AddToEnvironmentTemperature( GetTemperature() );
+				}
+			}
+		}
+	}
+};
